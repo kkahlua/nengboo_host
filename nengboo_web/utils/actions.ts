0f17a7a73593afd1d2df7ead27392bf3284ecab8
kkahlua <csw93083@naver.com>
2024-05-06 01:18:33 +0900

    [Feat] 냉장고 생성

--- nengboo_web/utils/actions.ts
+++ nengboo_web/utils/actions.ts
@@ -26,6 +26,7 @@ export const kakaoLogin = async () => {
   });
 };
 
+// 디버깅 용 로그아웃 함수
 export const logout = async () => {
   const { error } = await supabase.auth.signOut();
   if (!!error) {
@@ -33,6 +34,7 @@ export const logout = async () => {
   } else console.log("logout >>>", error);
 };
 
+// 소셜 로그인 한 유저의 정보를 유저 table에 담는 함수
 export const updateUser = async () => {
   const {
     data: { user },
@@ -50,18 +52,19 @@ export const updateUser = async () => {
         },
       ])
       .select();
-
+    insertRefrige(user);
     if (!error) console.log(data);
     else console.log("updateUser >>>", error);
   }
 };
 
+// 유저 table에서 로그인한 유저의 db정보를 모두 가져오는 함수
 export const getUserInfo = async () => {
   const {
     data: { user },
   } = await supabase.auth.getUser();
   if (!!user) {
-    let { data, error } = await supabase
+    const { data, error } = await supabase
       .from("users")
       .select("*")
       .eq("user_id", user.identities[0].identity_data.provider_id);
@@ -70,5 +73,29 @@ export const getUserInfo = async () => {
       console.log(data);
       return data;
     } else console.log("getUserInfo >>>", error);
+  }
+};
+
+// 냉장고 생성
+export const insertRefrige = async (user) => {
+  const { data, error } = await supabase
+    .from("refrigerators")
+    .select("*")
+    .eq("user_id", user.identities[0].identity_data.provider_id);
+
+  if (data.length !== 0) console.log("유저가 이미 냉장고를 가지고 있습니다.");
+  else {
+    const { data: refrige, error } = await supabase
+      .from("refrigerators")
+      .insert([
+        {
+          user_id: user.identities[0].identity_data.provider_id,
+          refrige_create_day: user.identities[0].created_at,
+        },
+      ])
+      .select();
+
+    if (!error) console.log(refrige);
+    else console.log("insertRefrige >>>", error);
   }
 };
