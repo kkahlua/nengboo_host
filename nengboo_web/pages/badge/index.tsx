985d1afff0d2c732e0c3ffa42c43d68c80ceaa44
H-Yun95 <shxmqnr951@gmail.com>
2024-05-09 14:23:56 +0900

    [Minor] 버튼 추가

--- nengboo_web/pages/badge/index.tsx
+++ nengboo_web/pages/badge/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
 import { BadgeData } from "@/utils/badgeData";
 import BadgeIcons from "@/components/badge/badge";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const index = () => {
   return (
     <div>
-      <div className="w-[428px] h-[816px] bg-white grid grid-cols-3 justify-items-center">
+      <div className="w-[428px] h-[600px] bg-white grid grid-cols-3 justify-items-center">
         {BadgeData.map((data) => (
           <BadgeIcons data={data} />
         ))}
+      </div>
+      <div className="w-[100%] h-[50px] grid place-items-center">
+        <Link href={"/badge/1"} className="w-[70%]">
+          <Button className="w-[100%] bg-sky-500 hover:bg-sky-600">
+            나무 보러가기
+          </Button>
+        </Link>
       </div>
     </div>
   );
