6c6291d647ccb035307a87319fa9dd3a7399557a
kkahlua <csw93083@naver.com>
2024-05-13 22:22:01 +0900

    [Feat] Recipe

--- nengboo_web/pages/recipe/index.tsx
+++ nengboo_web/pages/recipe/index.tsx
@@ -1,7 +1,132 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import arrowLeft from "@/public/arrowLeft.svg";
+import { Checkbox } from "@/components/ui/checkbox";
+import { getGPTRecipe, getRecipeImage } from "@/utils/actions";
 
-const index = () => {
-  return <div>Recipe Page</div>;
+const recipe = () => {
+  const [recipeName, setRecipeName] = useState<string>();
+  const [ingredient, setIngredient] = useState<string[]>();
+  const [cook, setCook] = useState<string[]>();
+  const [keyword, setKeyword] = useState<string>("");
+  const [recipeImage, setRecipeImage] = useState<string>();
+
+  const parseData = (recipeString: string) => {
+    const recipeNameRegex = /레시피 이름: (.*)\n/;
+    const recipeName = recipeString.match(recipeNameRegex)![1];
+
+    const ingredientsRegex = /재료: (.*)\n조리방법:/;
+    const ingredients = recipeString.match(ingredientsRegex)![1].split(", ");
+
+    const instructionsRegex = /조리방법: ((?:\d+\. .*?\n))/;
+    const instructions = recipeString
+      .match(instructionsRegex)![1]
+      .split(". ")
+      .filter((e) => e.length > 1);
+
+    const englishNameRegex = /레시피 영어이름: (.*)/;
+    const englishName = recipeString.match(englishNameRegex)![1];
+
+    setRecipeName(recipeName);
+    setKeyword(englishName);
+    setIngredient(ingredients);
+    setCook(instructions);
+  };
+
+  useEffect(() => {
+    const init = async () => {
+      const data = await getGPTRecipe();
+      parseData(data);
+    };
+    init();
+  }, []);
+
+  useEffect(() => {
+    if (!keyword) return;
+
+    const getImage = async () => {
+      const recipeImg = await getRecipeImage(keyword);
+      setRecipeImage(recipeImg);
+    };
+    getImage();
+  }, [keyword]);
+  return (
+    <main className="mx-6">
+      <Header />
+      <div className="flex items-center justify-center">
+        <img className="w-[334px] h-[334px]" src={recipeImage} />
+      </div>
+
+      <p className="mt-6 text-neutral-900 text-2xl font-bold">{recipeName}</p>
+
+      <SubHeader title={"재료"} />
+      <ul>
+        {ingredient?.map((e) => (
+          <li className="flex items-center space-x-2" key={e}>
+            <Checkbox id="e" key={e} />
+            <label
+              htmlFor="terms1"
+              className="text-neutral-500 text-base font-medium "
+            >
+              {e}
+            </label>
+          </li>
+        ))}
+      </ul>
+
+      <SubHeader title={"조리법"} />
+      <ul>
+        {cook?.map((e) => (
+          <li className="flex items-center space-x-2" key={e}>
+            <Checkbox id="e" key={e} />
+            <label
+              htmlFor="terms1"
+              className="text-neutral-500 text-base font-medium "
+            >
+              {e}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 };
 
-export default index;
+const Header = () => {
+  const router = useRouter();
+
+  return (
+    <div className="flex mt-16 mb-14">
+      <Image
+        src={arrowLeft}
+        alt="BackButton"
+        className="mr-2"
+        width={9}
+        height={18}
+        onClick={() => router.back()}
+      />
+      <div className="relative grow justify-center">
+        <div className="text-neutral-900 text-2xl font-bold text-center">
+          AI 추천 레시피
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const SubHeader = (title: { title: string }) => {
+  return (
+    <div className="flex items-center justify-center mb-8">
+      <div className="w-[140px] h-9 p-2 bg-neutral-100 rounded-[100px] justify-center items-center gap-2.5 inline-flex mt-32">
+        <div className="w-[113px] h-[22px] justify-center items-center flex">
+          <p className="w-[113px] h-[22px] text-center text-neutral-800 text-xl font-medium ">
+            {title.title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default recipe;
--- nengboo_web/utils/actions.ts
+++ nengboo_web/utils/actions.ts
@@ -1,5 +1,6 @@
 import { supabase } from "@/utils/supabase";
 import { sendMessage } from "./message";
+import OpenAI from "openai";
 
 export const googleLogin = async () => {
   sendMessage({ message: "login" });
@@ -115,4 +116,67 @@ export const insertRefrige = async () => {
       else console.log("insertRefrige >>>", error);
     }
   }
+};
+
+export const getProductList = async () => {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!!user && !!user.identities && !!user.identities[0].identity_data) {
+    const { data: ref_id, error } = await supabase
+      .from("refrigerators")
+      .select("refrige_id")
+      .eq("user_id", user.identities[0].identity_data.provider_id);
+    if (!!ref_id) {
+      const { data, error } = await supabase
+        .from("products")
+        .select("product_name")
+        .eq("refrige_id", ref_id[0].refrige_id);
+
+      if (!error) {
+        console.log(data);
+        return data;
+      } else console.log("getProdcutList >>>", error);
+    } else console.log("getProdcutList (ref_id) >>>", error);
+  } else console.log("getProdcutList >>> userError");
+};
+
+export const getGPTRecipe = async () => {
+  let parse = "";
+  const data = await getProductList();
+  console.log("data >>>", data);
+  if (!!data) {
+    data.map((e) => {
+      parse += e.product_name + " ";
+    });
+  }
+  const openai = new OpenAI({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: `${parse}들어간 레시피 1개를 '레시피 이름, 재료, 조리방법, 레시피 영어이름'만 추천하고 인사하지마`,
+      },
+    ],
+    temperature: 0,
+    max_tokens: 1000,
+  });
+  console.log(response);
+  return response.choices[0].message.content;
+};
+
+export const getRecipeImage = async (query: string) => {
+  const Access_Key = process.env.NEXT_PUBLIC_ACCESS_KEY;
+  const response = await fetch(
+    `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${Access_Key}&orientation=landscape&per_page=1`
+  );
+  const responseJson = await response.json();
+  const result = responseJson.results;
+  console.log("result >>>", result);
+  return result[0].urls.full;
 };
